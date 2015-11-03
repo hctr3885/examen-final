8278711a320c9aec73d33935efda445d01a53447
hector rojas <hrodrigo80@gmail.com>
2015-11-03 11:40:00 -0300

    modificacion de menu nosotros

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,3 @@
-  WebFontConfig = {
-    google: { families: [ 'Raleway::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })(); 
+ $.validate({
+    form : '#formulario'
+ });
